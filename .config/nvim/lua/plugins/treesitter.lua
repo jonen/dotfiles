ebbed2d0364f6b7a940d96dc8a1de1b87ae7c420
Jonathan Gross <jongr@me.com>
2024-02-16 13:07:30 +0100

    new selfmade nvim config

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return  {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {"c", "lua", "query", "vim", "vimdoc", "python"},
+      indent = { enable = true },
+      highlight = { enable = true },
+    })
+  end
+}
---
+++ .config/nvim/lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2 
+vim.opt.shiftwidth = 2
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<C-h>', '<C-w>h',{noremap = true})
+vim.keymap.set('n', '<C-j>', '<C-w>j',{noremap = true})
+vim.keymap.set('n', '<C-k>', '<C-w>k',{noremap = true})
+vim.keymap.set('n', '<C-l>', '<C-w>l',{noremap = true})
+
