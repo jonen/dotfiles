527e058778664aadc7f614e8c64963d919fb9be9
Jonathan Gross <jongr@me.com>
2024-02-16 14:14:12 +0100

    bufferline

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -5,10 +5,14 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.cursorline = true
 vim.g.mapleader = " "
---
+
 -- remap window navigation
 vim.keymap.set('n', '<C-h>', '<C-w>h',{noremap = true})
 vim.keymap.set('n', '<C-j>', '<C-w>j',{noremap = true})
 vim.keymap.set('n', '<C-k>', '<C-w>k',{noremap = true})
 vim.keymap.set('n', '<C-l>', '<C-w>l',{noremap = true})
+
+-- use Tab to switch buffers
+vim.keymap.set('n', '<tab>', ':bn<CR>',{noremap = true})
+vim.keymap.set('n', '<S-tab>', ':bp<CR>',{noremap = true})
 
