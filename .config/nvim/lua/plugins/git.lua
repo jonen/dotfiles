e5e2496a8bfa68542655e55e20e9409c6adf67e2
Jonathan Gross <jongr@me.com>
2024-02-19 22:04:56 +0100

    switch to neogit

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,11 @@
 return {
-  {
-    "tpope/vim-fugitive"
+   {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+    },
+    config = true
   },
   {
     "lewis6991/gitsigns.nvim",
