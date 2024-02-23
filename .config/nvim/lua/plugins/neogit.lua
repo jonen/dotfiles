e6e2f0d3619caed411d88509c3192cb88fb3fd50
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2024-02-23 17:37:46 +0100

    git blame on by default with a shorter delay

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -22,7 +22,16 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 300,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
+      })
     end
   }
 }
