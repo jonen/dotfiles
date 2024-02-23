0765e9363201e3f9aa94cb0c3d13432f69b8251d
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2024-02-23 17:29:13 +0100

    show changed and git indicators

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -6,11 +6,27 @@ return {
   },
   init = function() vim.g.barbar_auto_setup = false end,
   opts = {
-    auto_hide = false,
+    auto_hide = 1,
     clickable = true,
     focus_on_close = 'left',
+    modified = {button = '●'},
+    no_name_title = 'Untitled',
     sidebar_filetypes = {
       ['neo-tree'] = {event = 'BufWipeout'},
+    },
+    icons = {
+      buffer_index = true,
+      gitsigns = {
+        added = {enabled = true, icon = '+'},
+        changed = {enabled = true, icon = '~'},
+        deleted = {enabled = true, icon = '-'},
+      },
+      diagnostics = {
+        [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+        [vim.diagnostic.severity.WARN] = {enabled = false},
+        [vim.diagnostic.severity.INFO] = {enabled = false},
+        [vim.diagnostic.severity.HINT] = {enabled = true},
+      },
     }
   },
   version = '^1.0.0',
