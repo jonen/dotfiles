f317242b6bbc329ebc58158d353c0fe98b5ee19e
Jonathan Gross <jongr@me.com>
2024-06-17 21:24:37 +0200

    fix arch detection add zsh to arch

--- setup.sh
+++ setup.sh
@@ -5,9 +5,9 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     # macOS
     brew install git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" ~= "arch" ]]; then
+elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" == *"arch"* ]]; then
     # Arch Linux
-    sudo pacman -S git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh
+    sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
     echo "Unsupported operating system."
