3f1f6729e7b7763de93acf5264f7408855798558
Jonathan Gross <jongr@me.com>
2024-06-26 11:27:25 +0200

    added npm to arch linux setup

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" == *"arch"* ]]; then
     # Arch Linux
-    sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh eza stow npm openmp llvm clang gcc lua luarocks make wget
+    sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh eza stow npm openmp llvm clang gcc lua luarocks make wget npm
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
     echo "Unsupported operating system."
