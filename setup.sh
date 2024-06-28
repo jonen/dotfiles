1aa057cb97c2942ee0aed78f63b9e8b4e202218e
Jonathan Gross <jongr@me.com>
2024-06-28 23:29:17 +0200

    inital tpm clone to XDG location

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 if [[ "$(uname -s)" == "Darwin" ]]; then
     # macOS
     brew install git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide eza stow
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" == *"arch"* ]]; then
     # Arch Linux
     sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh eza stow npm openmp llvm clang gcc lua luarocks make wget jq yazi npm
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 else
     echo "Unsupported operating system."
     exit 1
