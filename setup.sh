b1ddeaa05d9c800952755f9ca39ac9d9bf15b020
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2024-07-26 08:51:26 +0200

    added git-delta to config

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     if [[ ! $(command -v brew) ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-    brew install git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide eza stow font-meslo-lg-nerd-font bat yazi jq
+    brew install git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide eza stow font-meslo-lg-nerd-font bat yazi jq git-delta
     git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
     bat cache --build
 elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" == *"arch"* ]]; then
     # Arch Linux
-    sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh eza stow npm openmp llvm clang gcc lua luarocks make wget jq yazi npm cargo rust bat ttf-meslo-nerd
+    sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh eza stow npm openmp llvm clang gcc lua luarocks make wget jq yazi npm cargo rust bat ttf-meslo-nerd git-delta
     git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
     bat cache --build
 else
