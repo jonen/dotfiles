226b0bae1e114c0e30ac050784ee7ab01b70513f
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2025-01-15 08:32:09 +0100

    file formatting

--- nvim/dot-config/nvim/lua/vim-options.lua
+++ nvim/dot-config/nvim/lua/vim-options.lua
@@ -19,37 +19,45 @@ vim.keymap.set("v", ">", ">gv")
 -- replace doesn't copy to clipboard
 vim.keymap.set("x", "p", [["_dP]])
 
-vim.keymap.set("n", "<leader>tc", ":setlocal <C-R>=&conceallevel ? 'conceallevel=0' : 'conceallevel=2'<CR><CR>",
-  { desc = "[T]oggle [C]onceallevel" })
+vim.keymap.set(
+	"n",
+	"<leader>tc",
+	":setlocal <C-R>=&conceallevel ? 'conceallevel=0' : 'conceallevel=2'<CR><CR>",
+	{ desc = "[T]oggle [C]onceallevel" }
+)
 
 -- set clipboard
 vim.opt.clipboard = "unnamedplus"
 
 -- markdown
 vim.g.markdown_fenced_languages = {
-  "html",
-  "python",
-  "lua",
-  "vim",
-  "typescript",
-  "javascript",
-  "sh",
-  "bash",
-  "cpp",
-  "lisp",
-  "lua",
-  "make",
-  "cmake",
-  "rust",
+	"html",
+	"python",
+	"lua",
+	"vim",
+	"typescript",
+	"javascript",
+	"sh",
+	"bash",
+	"cpp",
+	"lisp",
+	"lua",
+	"make",
+	"cmake",
+	"rust",
 }
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
+
+-- Enable blinking cursor in insert mode
+vim.opt.guicursor =
+	"n-v-c-sm:block,i-ci-ve:block-blinkwait300-blinkon200-blinkoff150,r-cr-o:hor20-blinkwait300-blinkon200-blinkoff150"
