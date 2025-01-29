010267ba390c6ad04b542d9c08aa968442ffa9e1
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2025-01-29 21:52:03 +0100

    added support for el9

--- setup.sh
+++ setup.sh
@@ -14,6 +14,25 @@ elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" == *"arch"* ]]; then
     sudo pacman -Syu git tmux neovim ripgrep fd poetry starship lazygit fzf zoxide zsh eza stow npm openmp llvm clang gcc lua luarocks make wget jq yazi npm cargo rust bat ttf-meslo-nerd git-delta
     git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
     bat cache --build
+elif [[ "$(uname -s)" == "Linux" && "$(uname -r)" == *"el9"* ]]; then
+    # RHEL9
+    # add Repos
+    # for fd
+    sudo dnf copr enable tkbcopr/fd -y
+    # starship
+    sudo dnf copr enable atim/starship -y
+    # lazygit
+    sudo dnf copr enable atim/lazygit -y
+    sudo dnf install -y git tmux neovim ripgrep fd python3 starship lazygit fzf zoxide zsh stow npm llvm clang gcc lua luarocks make wget jq npm cargo bat git-delta
+    # rust and cargo
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup update
+    # install eza
+    cargo install eza
+    # install yazi
+    cargo install yazi-fm yazi-cli
+    git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+    bat cache --build
 else
     echo "Unsupported operating system."
     exit 1
