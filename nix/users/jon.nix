1671b081194d62e1fefd50987a9ad59f446a2ab7
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2025-05-31 17:37:18 +0200

    fixed homeDir

--- nix/users/jon.nix
+++ nix/users/jon.nix
@@ -1,14 +1,21 @@
 
 { config, pkgs, ... }:
 
+let
+  homeDir = if pkgs.stdenv.isDarwin then
+    "/Users/jon"
+  else
+    "/home/jon";
+
+in
 {
   home.username = "jon";
-  home.homeDirectory = "/Users/jon";
+  home.homeDirectory = homeDir;
 	
   home.stateVersion = "25.05";
 
   home.packages = [
-    pkgs.home-manager
+#    pkgs.home-manager
   ];
 
   home.file.".zshrc".source = ../../zsh/dot-zshrc;
