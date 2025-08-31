179fdf95382a5bba30b3d08a42cd406ed0d9adda
Jonathan Gross <jongr@me.com>
2025-08-31 19:03:03 +0200

    fixed syntax for mas packages

--- nix/nix-darwin/configuration.nix
+++ nix/nix-darwin/configuration.nix
@@ -20,10 +20,10 @@
 
   system.activationScripts.masApps.text = ''
     # Add more app IDs as needed
-    462058435   # Microsoft Excel
-    784801555   # Microsoft OneNote
-    985367838   # Microsoft Outlook
-    462062816   # Microsoft PowerPoint
-    462054704   # Microsoft Word
+    mas install 462058435   # Microsoft Excel
+    mas install 784801555   # Microsoft OneNote
+    mas install 985367838   # Microsoft Outlook
+    mas install 462062816   # Microsoft PowerPoint
+    mas install 462054704   # Microsoft Word
   '';
 }
