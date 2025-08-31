98b527c314e032179ecf6df5c87107dc18226051
Jonathan Gross <jongr@me.com>
2025-08-31 17:31:42 +0200

    readded homebrew

--- nix/nix-darwin/configuration.nix
+++ nix/nix-darwin/configuration.nix
@@ -1,20 +1,21 @@
-{ pkgs, self, ... }: {
+{ pkgs, self, ... }:
+{
 
-    nix.settings.experimental-features = "nix-command flakes";
+  nix.settings.experimental-features = "nix-command flakes";
 
-    # Set Git commit hash for darwin-version.
-    system.configurationRevision = self.rev or self.dirtyRev or null;
-    system.primaryUser = "jon";
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+  system.primaryUser = "jon";
 
-    # Used for backwards compatibility, please read the changelog before changing.
-    # $ darwin-rebuild changelog
-    system.stateVersion = 6;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
 
-    # The platform the configuration will be used on.
-    nixpkgs.hostPlatform = "aarch64-darwin";
-    nixpkgs.config.allowUnfree = true;
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+  nixpkgs.config.allowUnfree = true;
 
-    # Install the following packages
-    environment.systemPackages = import ./nix-packages.nix pkgs;
-    # homebrew = import ./homebrew-packages.nix;
+  # Install the following packages
+  environment.systemPackages = import ./nix-packages.nix pkgs;
+  homebrew = import ./homebrew-packages.nix;
 }
