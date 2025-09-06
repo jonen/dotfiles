cbd1f399b89545ad8c90e75c15de1a7e7effe5b8
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2025-09-06 12:45:37 +0200

    fixed links to dotfiles

--- home.nix
+++ home.nix
@@ -11,22 +11,21 @@
   ];
 
   home.file = {
-    ".bashrc".source = ../../bash/dot-bashrc;
-    ".config/lazygit".source = ../../lazygit/dot-config/lazygit;
-    ".config/yazi".source = ../../yazi/dot-config/yazi;
-    ".config/nvim".source = ../../nvim/dot-config/nvim;
-    ".config/kitty".source = ../../kitty/dot-config/kitty;
-    ".config/ghostty".source = ../../ghostty/dot-config/ghostty;
-    ".config/btop".source = ../../btop/dot-config/btop;
-    ".config/bat".source = ../../bat/dot-config/bat;
-    ".config/tmux/tmux.conf".source = ../../tmux/dot-config/tmux/tmux.conf;
-    ".inputrc".source = ../../zsh/dot-inputrc;
-    ".zprofile".source = ../../zsh/dot-zprofile;
-    ".gitconfig".source = ../../git/dot-gitconfig;
-    ".gitignore_global".source = ../../git/dot-gitignore_global;
-    ".zshrc".source = config.lib.file.mkOutOfStoreSymlink ./zsh/dot-zshrc;
-    ".config/starship.toml".source =
-      config.lib.file.mkOutOfStoreSymlink ./starship/dot-config/starship.toml;
-    ".local/bin/oil-ssh.sh".source = ../../scripts/dot-local/bin/oil-ssh.sh;
+    ".bashrc".source = ./bash/dot-bashrc;
+    ".config/lazygit".source = ./lazygit/dot-config/lazygit;
+    ".config/yazi".source = ./yazi/dot-config/yazi;
+    ".config/nvim".source = ./nvim/dot-config/nvim;
+    ".config/kitty".source = ./kitty/dot-config/kitty;
+    ".config/ghostty".source = ./ghostty/dot-config/ghostty;
+    ".config/btop".source = ./btop/dot-config/btop;
+    ".config/bat".source = ./bat/dot-config/bat;
+    ".config/tmux/tmux.conf".source = ./tmux/dot-config/tmux/tmux.conf;
+    ".inputrc".source = ./zsh/dot-inputrc;
+    ".zprofile".source = ./zsh/dot-zprofile;
+    ".gitconfig".source = ./git/dot-gitconfig;
+    ".gitignore_global".source = ./git/dot-gitignore_global;
+    ".zshrc".source = ./zsh/dot-zshrc;
+    ".config/starship.toml".source = ./starship/dot-config/starship.toml;
+    ".local/bin/oil-ssh.sh".source = ./scripts/dot-local/bin/oil-ssh.sh;
   };
 }
