0a2ebbfd3af506e097f4c4aaed37a367ce4e5089
Jonathan Gross <jonathan.gross@uni-leipzig.de>
2025-11-02 16:40:52 +0100

    new whichkey syntax

--- nvim/dot-config/nvim/lua/plugins/which-key.lua
+++ nvim/dot-config/nvim/lua/plugins/which-key.lua
@@ -1,143 +1,106 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 200
-	end,
-	opts = {
-		preset = "helix",
-	},
-	-- keys = {
-	-- 	{ "<leader>b", desc = "buffer" },
-	-- 	{ "<leader>bb", "<CMD>Telescope buffers<CR>", desc = "Show buffer list" },
-	-- 	{ "<leader>bc", ":bd<CR>", desc = "Close current buffer" },
-	-- 	{ "<leader>bd", ":bd<CR>", desc = "Close current buffer" },
-	-- 	{ "<leader>bi", "gg=G``", desc = "Indent current buffer" }, -- indent and keep current cursor position
-	-- 	{ "<leader>bo", "<CMD>BufferCloseAllButCurrent<CR>|'\"", desc = "Close all other buffers" },
-	-- 	{ "<leader>bn", "<CMD>bn<CR>", desc = "Next buffer" },
-	-- 	{ "<leader>bp", "<CMD>bp<CR>", desc = "Previous buffer" },
-	-- },
-	config = function()
-		require("which-key").register({
-			["<leader>b"] = {
-				name = "+buffer",
-				b = { "<CMD>Telescope buffers<CR>", "Show buffer list" },
-				c = { ":bd<CR>", "Close current buffer" },
-				d = { ":bd<CR>", "Close current buffer" },
-				i = { "gg=G``", "Indent current buffer" }, -- indent and keep current cursor position
-				l = { "<CMD>Telescope buffers<CR>", "Show buffer list" },
-				o = { "<CMD>BufferCloseAllButCurrent<CR>|'\"", "Close all other buffers" },
-				n = { "<CMD>bn<CR>", "Next buffer" },
-				p = { "<CMD>bp<CR>", "Previous buffer" },
-				["1"] = { "<CMD>BufferGoto 1<CR>", "Goto buffer 1" },
-				["2"] = { "<CMD>BufferGoto 2<CR>", "Goto buffer 2" },
-				["3"] = { "<CMD>BufferGoto 3<CR>", "Goto buffer 3" },
-				["4"] = { "<CMD>BufferGoto 4<CR>", "Goto buffer 4" },
-				["5"] = { "<CMD>BufferGoto 5<CR>", "Goto buffer 5" },
-				["6"] = { "<CMD>BufferGoto 5<CR>", "Goto buffer 6" },
-				["7"] = { "<CMD>BufferGoto 5<CR>", "Goto buffer 7" },
-				["8"] = { "<CMD>BufferGoto 5<CR>", "Goto buffer 8" },
-				["9"] = { "<CMD>BufferGoto 5<CR>", "Goto buffer 9" },
-				["0"] = { "<CMD>BufferLast<CR>", "Goto last buffer" },
-			},
-			["<leader>c"] = {
-				name = "+code",
-				a = { vim.lsp.buf.code_action, "Code actions" },
-				g = {
-					name = "+goto",
-					d = { vim.lsp.buf.definition, "Goto definition" },
-					D = { vim.lsp.buf.declaration, "Goto declaration" },
-					i = { vim.lsp.buf.implementation, "Goto implementation" },
-					n = { vim.diagnostic.goto_next, "Goto next diagnostic" },
-					p = { vim.diagnostic.goto_prev, "Goto previous diagnostic" },
-					r = { vim.lsp.buf.references, "List references" },
-				},
-				i = { vim.lsp.buf.hover, "Show information" },
-				r = { vim.lsp.buf.rename, "Rename symbol" },
-			},
-			["<leader>e"] = {
-				name = "+edit",
-				c = {
-					function()
-						require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
-					end,
-					"Edit neovim config",
-				},
-				d = {
-					function()
-						require("telescope.builtin").find_files({ cwd = "~/dotfiles" })
-					end,
-					"Edit dotfiles",
-				},
-			},
-			["<leader>f"] = {
-				name = "+file",
-				["/"] = { "<CMD>Telescope current_buffer_fuzzy_find<CR>", "Find in file" },
-				f = { "<CMD>Telescope find_files<CR>", "Find file" },
-				g = { "<CMD>Telescope live_grep<CR>", "Grep files" },
-				n = { "<CMD>enew<cr>", "New File" },
-				o = { "<CMD>Oil<CR>", "Open Oil" },
-				r = { "<CMD>Telescope oldfiles<cr>", "Open recent file" },
-				s = { ":w<CR>", "Save current file" },
-			},
-			["<leader>g"] = {
-				name = "+git",
-				b = { ":Gitsigns toggle_current_line_blame<CR>", "Toggle git blame on line" },
-				s = { ":Neogit<CR>", "Neogit status" },
-			},
-			["<leader>n"] = {
-				name = "+notifications",
-				d = {
-					function()
-						require("notify").dismiss({ silent = true, pending = true })
-					end,
-					"Dismiss all notifications",
-				},
-				s = { "<CMD>Telescope notify<CR>", "Show all notifications" },
-			},
-			["<leader>o"] = {
-				name = "Obsidian",
-				b = { "<CMD>ObsidianBacklinks<CR>", "Backlinks" },
-				c = { "<CMD>ObsidianToggleCheckbox<CR>", "Toggle Checkbox" },
-				d = { "<CMD>ObsidianDailies<CR>", "Daily notes" },
-				f = { "<CMD>ObsidianSearch<CR>", "Find notes" },
-				l = { "<CMD>ObsidianLinks<CR>", "Links" },
-				n = { "<CMD>ObsidianNew<CR>", "New note" },
-				o = { "<CMD>ObsidianOpen<CR>", "Open Obsidian.app" },
-				t = { "<CMD>ObsidianToday<CR>", "Today" },
-				w = { "<CMD>ObsidianWorkspace<CR>", "Change workspace" },
-				y = { "<CMD>ObsidianYesterday<CR>", "Yesterday" },
-			},
-			["<leader>q"] = {
-				name = "+quit",
-				f = { ":qa!<CR>", "Quit (force, no save)" },
-				q = { ":qa<CR>", "Quit" },
-				s = { ":wqa<CR>", "Quit (and save all)" },
-			},
-			["<leader>s"] = {
-				name = "+search",
-				d = { "<CMD>set hlsearch!<CR>", "Toggle search highlight" },
-			},
-			["<leader>u"] = {
-				name = "+undo",
-				t = { "<cmd>lua require('undotree').toggle()<cr>", "Toggle undotree" },
-			},
-			["<leader>v"] = {
-				name = "+venv",
-				c = { "<cmd>VenvSelectCached<CR>", "Select cached venv" },
-				s = { "<cmd>VenvSelect<CR>", "Select venv" },
-			},
-			["<leader>w"] = {
-				name = "+window",
-				c = { "<C-w>q", "Close current window" },
-				o = { "<C-w>o", "Close all other windows" },
-				s = {
-					name = "+split",
-					v = { ":vsplit<CR>", "Split window vertically" },
-					h = { ":split<CR>", "Split window horizontally" },
-				},
-			},
-		})
-	end,
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 200
+  end,
+  opts = {
+    preset = "helix",
+    spec = {
+      { "<leader>b", group = "buffer" },
+      { "<leader>b0", "<CMD>BufferLast<CR>", desc = "Goto last buffer" },
+      { "<leader>b1", "<CMD>BufferGoto 1<CR>", desc = "Goto buffer 1" },
+      { "<leader>b2", "<CMD>BufferGoto 2<CR>", desc = "Goto buffer 2" },
+      { "<leader>b3", "<CMD>BufferGoto 3<CR>", desc = "Goto buffer 3" },
+      { "<leader>b4", "<CMD>BufferGoto 4<CR>", desc = "Goto buffer 4" },
+      { "<leader>b5", "<CMD>BufferGoto 5<CR>", desc = "Goto buffer 5" },
+      { "<leader>b6", "<CMD>BufferGoto 6<CR>", desc = "Goto buffer 6" },
+      { "<leader>b7", "<CMD>BufferGoto 7<CR>", desc = "Goto buffer 7" },
+      { "<leader>b8", "<CMD>BufferGoto 8<CR>", desc = "Goto buffer 8" },
+      { "<leader>b9", "<CMD>BufferGoto 9<CR>", desc = "Goto buffer 9" },
+      { "<leader>bb", "<CMD>Telescope buffers<CR>", desc = "Show buffer list" },
+      { "<leader>bc", ":bd<CR>", desc = "Close current buffer" },
+      { "<leader>bd", ":bd<CR>", desc = "Close current buffer" },
+      { "<leader>bi", "gg=G``", desc = "Indent current buffer" },
+      { "<leader>bl", "<CMD>Telescope buffers<CR>", desc = "Show buffer list" },
+      { "<leader>bn", "<CMD>bn<CR>", desc = "Next buffer" },
+      { "<leader>bo", "<CMD>BufferCloseAllButCurrent<CR>|'\"", desc = "Close all other buffers" },
+      { "<leader>bp", "<CMD>bp<CR>", desc = "Previous buffer" },
+      { "<leader>c", group = "code" },
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code actions" },
+      { "<leader>cg", group = "goto" },
+      { "<leader>cgD", vim.lsp.buf.declaration, desc = "Goto declaration" },
+      { "<leader>cgd", vim.lsp.buf.definition, desc = "Goto definition" },
+      { "<leader>cgi", vim.lsp.buf.implementation, desc = "Goto implementation" },
+      { "<leader>cgn", vim.diagnostic.goto_next, desc = "Goto next diagnostic" },
+      { "<leader>cgp", vim.diagnostic.goto_prev, desc = "Goto previous diagnostic" },
+      { "<leader>cgr", vim.lsp.buf.references, desc = "List references" },
+      { "<leader>ci", vim.lsp.buf.hover, desc = "Show information" },
+      { "<leader>cr", vim.lsp.buf.rename, desc = "Rename symbol" },
+      { "<leader>e", group = "edit" },
+      {
+        "<leader>ec",
+        function()
+          require("telescope.builtin").find_files({ cwd = vim.fn.stdpath("config") })
+        end,
+        desc = "Edit neovim config",
+      },
+      {
+        "<leader>ed",
+        function()
+          require("telescope.builtin").find_files({ cwd = "~/dotfiles" })
+        end,
+        desc = "Edit dotfiles",
+      },
+      { "<leader>f", group = "file" },
+      { "<leader>f/", "<CMD>Telescope current_buffer_fuzzy_find<CR>", desc = "Find in file" },
+      { "<leader>ff", "<CMD>Telescope find_files<CR>", desc = "Find file" },
+      { "<leader>fg", "<CMD>Telescope live_grep<CR>", desc = "Grep files" },
+      { "<leader>fn", "<CMD>enew<cr>", desc = "New File" },
+      { "<leader>fo", "<CMD>Oil<CR>", desc = "Open Oil" },
+      { "<leader>fr", "<CMD>Telescope oldfiles<cr>", desc = "Open recent file" },
+      { "<leader>fs", ":w<CR>", desc = "Save current file" },
+      { "<leader>g", group = "git" },
+      { "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", desc = "Toggle git blame on line" },
+      { "<leader>gs", ":Neogit<CR>", desc = "Neogit status" },
+      { "<leader>n", group = "notifications" },
+      {
+        "<leader>nd",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "Dismiss all notifications",
+      },
+      { "<leader>ns", "<CMD>Telescope notify<CR>", desc = "Show all notifications" },
+      { "<leader>o", group = "Obsidian" },
+      { "<leader>ob", "<CMD>ObsidianBacklinks<CR>", desc = "Backlinks" },
+      { "<leader>oc", "<CMD>ObsidianToggleCheckbox<CR>", desc = "Toggle Checkbox" },
+      { "<leader>od", "<CMD>ObsidianDailies<CR>", desc = "Daily notes" },
+      { "<leader>of", "<CMD>ObsidianSearch<CR>", desc = "Find notes" },
+      { "<leader>ol", "<CMD>ObsidianLinks<CR>", desc = "Links" },
+      { "<leader>on", "<CMD>ObsidianNew<CR>", desc = "New note" },
+      { "<leader>oo", "<CMD>ObsidianOpen<CR>", desc = "Open Obsidian.app" },
+      { "<leader>ot", "<CMD>ObsidianToday<CR>", desc = "Today" },
+      { "<leader>ow", "<CMD>ObsidianWorkspace<CR>", desc = "Change workspace" },
+      { "<leader>oy", "<CMD>ObsidianYesterday<CR>", desc = "Yesterday" },
+      { "<leader>q", group = "quit" },
+      { "<leader>qf", ":qa!<CR>", desc = "Quit (force, no save)" },
+      { "<leader>qq", ":qa<CR>", desc = "Quit" },
+      { "<leader>qs", ":wqa<CR>", desc = "Quit (and save all)" },
+      { "<leader>s", group = "search" },
+      { "<leader>sd", "<CMD>set hlsearch!<CR>", desc = "Toggle search highlight" },
+      { "<leader>u", group = "undo" },
+      { "<leader>ut", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle undotree" },
+      { "<leader>v", group = "venv" },
+      { "<leader>vc", "<cmd>VenvSelectCached<CR>", desc = "Select cached venv" },
+      { "<leader>vs", "<cmd>VenvSelect<CR>", desc = "Select venv" },
+      { "<leader>w", group = "window" },
+      { "<leader>wc", "<C-w>q", desc = "Close current window" },
+      { "<leader>wo", "<C-w>o", desc = "Close all other windows" },
+      { "<leader>ws", group = "split" },
+      { "<leader>wsh", ":split<CR>", desc = "Split window horizontally" },
+      { "<leader>wsv", ":vsplit<CR>", desc = "Split window vertically" },
+    },
+  },
 }
